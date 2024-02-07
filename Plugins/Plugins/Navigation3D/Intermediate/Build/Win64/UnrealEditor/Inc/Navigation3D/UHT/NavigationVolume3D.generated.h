68ee00dfbcc61caa3e31fcb293f6837c5156e511
RafotRarran <122271977+RafotRarran@users.noreply.github.com>
2024-02-07 16:30:19 +0000

    Added Plugin

---
+++ Plugins/Plugins/Navigation3D/Intermediate/Build/Win64/UnrealEditor/Inc/Navigation3D/UHT/NavigationVolume3D.generated.h
@@ -0,0 +1,119 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "NavigationVolume3D.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+class UObject;
+#ifdef NAVIGATION3D_NavigationVolume3D_generated_h
+#error "NavigationVolume3D.generated.h already included, missing '#pragma once' in NavigationVolume3D.h"
+#endif
+#define NAVIGATION3D_NavigationVolume3D_generated_h
+
+#define FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_SPARSE_DATA
+#define FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execGetDivisionSize); \
+	DECLARE_FUNCTION(execGetDivisionsZ); \
+	DECLARE_FUNCTION(execGetDivisionsY); \
+	DECLARE_FUNCTION(execGetDivisionsX); \
+	DECLARE_FUNCTION(execGetTotalDivisions); \
+	DECLARE_FUNCTION(execConvertCoordinatesToLocation); \
+	DECLARE_FUNCTION(execConvertLocationToCoordinates); \
+	DECLARE_FUNCTION(execFindPath);
+
+
+#define FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execGetDivisionSize); \
+	DECLARE_FUNCTION(execGetDivisionsZ); \
+	DECLARE_FUNCTION(execGetDivisionsY); \
+	DECLARE_FUNCTION(execGetDivisionsX); \
+	DECLARE_FUNCTION(execGetTotalDivisions); \
+	DECLARE_FUNCTION(execConvertCoordinatesToLocation); \
+	DECLARE_FUNCTION(execConvertLocationToCoordinates); \
+	DECLARE_FUNCTION(execFindPath);
+
+
+#define FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_ACCESSORS
+#define FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesANavigationVolume3D(); \
+	friend struct Z_Construct_UClass_ANavigationVolume3D_Statics; \
+public: \
+	DECLARE_CLASS(ANavigationVolume3D, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Navigation3D"), NO_API) \
+	DECLARE_SERIALIZER(ANavigationVolume3D)
+
+
+#define FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesANavigationVolume3D(); \
+	friend struct Z_Construct_UClass_ANavigationVolume3D_Statics; \
+public: \
+	DECLARE_CLASS(ANavigationVolume3D, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Navigation3D"), NO_API) \
+	DECLARE_SERIALIZER(ANavigationVolume3D)
+
+
+#define FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ANavigationVolume3D(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ANavigationVolume3D) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ANavigationVolume3D); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ANavigationVolume3D); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ANavigationVolume3D(ANavigationVolume3D&&); \
+	NO_API ANavigationVolume3D(const ANavigationVolume3D&); \
+public: \
+	NO_API virtual ~ANavigationVolume3D();
+
+
+#define FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ANavigationVolume3D(ANavigationVolume3D&&); \
+	NO_API ANavigationVolume3D(const ANavigationVolume3D&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ANavigationVolume3D); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ANavigationVolume3D); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ANavigationVolume3D) \
+	NO_API virtual ~ANavigationVolume3D();
+
+
+#define FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_12_PROLOG
+#define FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_SPARSE_DATA \
+	FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_RPC_WRAPPERS \
+	FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_ACCESSORS \
+	FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_INCLASS \
+	FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_SPARSE_DATA \
+	FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_ACCESSORS \
+	FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_INCLASS_NO_PURE_DECLS \
+	FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> NAVIGATION3D_API UClass* StaticClass<class ANavigationVolume3D>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_arran_Documents_UltrarenGitFixed_UltrarenMaster1901_Github_2_0_Plugins_Plugins_Navigation3D_Source_Navigation3D_Public_NavigationVolume3D_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
